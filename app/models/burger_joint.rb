884cc591685f28ba4dee52747992b0c5b59c0cd3
github username <therealgracechoi@gmail.com>
2020-06-26 16:40:58 -0700

    added some logic to BurgerJoint.rb

--- app/models/burger_joint.rb
+++ app/models/burger_joint.rb
@@ -1,2 +1,14 @@
 class BurgerJoint < ApplicationRecord
+    has_many :reviews
+
+    # this slugifies the names of the burgerjoints before creating it in our db:
+    before_create :slugify
+
+    def slugify
+        self.slug = name.parameterize
+    end
+
+    def avg_score
+        reviews.average(:score).round(2).to_f
+    end
 end
